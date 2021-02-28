12f1b7841317975172526a5ec3da2385ff22814f
Walter Ferreira <FerreiraWalter@users.noreply.github.com>
2021-02-28 13:11:39 -0300

    Inicio BOT GUITARFLASH

---
+++ README.md
@@ -0,0 +1,2 @@
+# GuitarFlash-BOT
+ 
---
+++ guitarflashbot.py
@@ -0,0 +1,71 @@
+from PIL import ImageGrab
+import pyautogui
+import time
+
+#FUNCTIONS
+def capture_screen(): #FUNCIONANDO
+    screen = ImageGrab.grab()
+    return screen
+
+#GREEN
+def detected_green(screen):
+    color = screen.getpixel((787, 784))
+    if color == (0, 152, 0) or color == (255, 255, 255) or color == (1, 1, 1):
+        return True
+
+def press_green():
+    pyautogui.press("A");
+
+#RED
+def detected_red(screen):
+    color = screen.getpixel((878, 779))
+    if color == (255, 0, 0) or color == (255, 255, 255) or color == (1, 1, 1):
+        return True
+
+def press_red():
+    pyautogui.press("S");
+
+#YELLOW
+def detected_yellow(screen):
+    color = screen.getpixel((971, 785))
+    if color == (244, 244, 2) or color == (255, 255, 255) or color == (1, 1, 1):
+        return True
+
+def press_yellow():
+    pyautogui.press("J");
+
+#BLUE
+def detected_blue(screen):
+    color = screen.getpixel((1065, 785))
+    if color == (0, 152, 255) or color == (255, 255, 255) or color == (1, 1, 1):
+        return True
+
+def press_blue():
+    pyautogui.press("K");
+
+#ORANGE
+def detected_orange(screen):
+    color = screen.getpixel((1157, 785))
+    if color == (255, 101, 0) or color == (255, 255, 255) or color == (1, 1, 1):
+        return True
+
+def press_orange():
+    pyautogui.press("L");
+
+#MAIN PROGRAM
+print('Starting in 2 seconds...')
+time.sleep(2)
+
+
+while True:
+    screen = capture_screen();
+    if detected_green(screen):
+        press_green()
+    if detected_red(screen):
+        press_red()
+    if detected_yellow(screen):
+        press_yellow()
+    if detected_blue(screen):
+        press_blue()
+    if detected_orange(screen):
+        press_orange()
